74597e6282859317415c242b7ce379542cc0d800
Muhammad Joynul Hussain <muhammadjoyhussain@gmail.com>
2021-10-10 22:14:58 +0600

    [ADD]

--- models/Product.js
+++ models/Product.js
@@ -3,23 +3,27 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Please add a name'],
         trim: true,
     },
     price: {
         type: Number,
-        required: true,
+        required: [true, 'Please add a price'],
     },
     description: {
         type: String,
-        required: true,
+        required: [true, 'Please add a description'],
     },
     image: {
         type: String,
-        required: true
+        required: [true, 'Please add an image']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     },
     store: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: mongoose.Schema.ObjectId,
         ref: 'Store',
         required: true,
     },
---
+++ seeder.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Store = require('./models/Store');
+const Product = require('./models/Product');
+const Category = require('./models/Category');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Read JSON files
+const store = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/Store.json`, 'utf-8')
+);
+
+const product = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/product.json`, 'utf-8')
+);
+
+const category = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/category.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+    try {
+        await Store.create(store);
+        await Product.create(product);
+        await Category.create(category);
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Store.deleteMany();
+        await Product.deleteMany();
+        await Category.deleteMany();
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+}
